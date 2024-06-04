dfcc101f64c825cb585df5843dd42632c6a29eed
MuhammadZaid1999 <zaid.aptechiic@gmail.com>
2024-06-05 01:52:04 +0500

    background setting

--- frontend/src/Pages/HomePage/HomePage.jsx
+++ frontend/src/Pages/HomePage/HomePage.jsx
@@ -1,8 +1,56 @@
 import React from "react";
+import { 
+    Box, 
+    Container, 
+    Text,
+    Tabs,
+    Tab,
+    TabList,
+    TabPanels,
+    TabPanel 
+} from "@chakra-ui/react";
 
 const HomePage = () => {
     return(
-        <div> Home Page </div>
+        <Container maxW="xl" centerContent>
+            <Box
+                d="flex"
+                alignContent="center"
+                textAlign="center"
+                p={3}
+                bg={"white"}
+                w="100%"
+                m="50px 0 15px 0"
+                borderRadius="lg"
+                borderWidth="1px"
+            >
+                <Text fontSize="4xl" fontFamily="Work sans">
+                    Tack-A-Tive
+                </Text>
+            </Box>
+            <Box 
+                bg="white" 
+                w="100%" 
+                p={4}
+                borderRadius="lg"
+                borderWidth="1px"
+            >
+                <Tabs variant='soft-rounded'>
+                    <TabList mb="1em">
+                        <Tab w="50%">Login</Tab>
+                        <Tab w="50%">Sign Up</Tab>
+                    </TabList>
+                    <TabPanels>
+                        <TabPanel>
+                        <p>one!</p>
+                        </TabPanel>
+                        <TabPanel>
+                        <p>two!</p>
+                        </TabPanel>
+                    </TabPanels>
+                </Tabs>
+            </Box>
+        </Container>
     )
 }
 
