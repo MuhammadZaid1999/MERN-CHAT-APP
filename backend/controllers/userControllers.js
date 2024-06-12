1ce43bdd9a52b645887b5ea549da68cc77e0b54b
MuhammadZaid1999 <zaid.aptechiic@gmail.com>
2024-06-12 18:36:25 +0500

    jwt header cerification

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -2,6 +2,7 @@ const asyncHandler = require("express-async-handler")
 const User = require("../models/userModel")
 const generateToken = require("../config/generateToken")
 
+
 const registerUser = asyncHandler(async (req, res) => {
     const {name, email, password, pic} = req.body
     
@@ -58,7 +59,22 @@ const authUser = asyncHandler(async (req, res) => {
     }
 })
 
+
+const allUsers = asyncHandler(async (req, res) => {
+    const keyword = req.query.search ? {
+        $or: [
+            {name: {$regex: req.query.search, $options: "i"}},
+            {email: {$regex: req.query.search, $options: "i"}}
+        ]
+    } : {}
+    
+    const users = await User.find(keyword).find({ _id: {$ne: req.user._id}}).select("-password")
+    res.send(users)
+    
+})
+
 module.exports = {
     registerUser,
-    authUser
+    authUser,
+    allUsers
 }
